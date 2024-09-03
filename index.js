31454d4879ca453debfce4e75a7978636000b493
Lucas Silva <59655760+lucasb-silva@users.noreply.github.com>
2024-09-03 00:18:18 +0000

    Banco de dados implementado com endpoint Create

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
 const express = require('express')
-const app = express()
+const { MongoClient, Collection } = require('mongodb')
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
 
-app.listen(3000)
+const dbUrl = 'mongodb+srv://' + dbUser + ':' + dbPassword + '@cluster0.zaxp3ht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const dbName = 'biblioteca'
+
+async function main() {
+    // Realizamos a conexão com o banco de dados
+    const client = new MongoClient(dbUrl)
+    console.log('Conectando ao banco de dados...')
+    await client.connect()
+    console.log('Banco de dados conectado com sucesso!')
+
+    // Declaração de variaveis para manipular o banco
+    const db = client.db(dbName)
+    const collection = db.collection('livros')
+
+    // Inicio o app
+    const app = express()
+    // Sinalizo para o Express que estamos usando JSON no Body
+    app.use(express.json())
+
+    app.post('/livros', async function (req, res) {
+        // Acessamos o body da requisição
+        const newItem = req.body
+
+        // Checamos se `livro` está presente na requisição
+        // if (!newItem || (!newItem.titulo && !newItem.autor && !newItem.anoPublicacao)) {
+        //     return res.status(400).send('Corpo da requisição deve conter a propriedade `titulo`.')
+        // }
+
+        // Adicionamos na collection
+        await collection.insertOne(newItem)
+
+        res.status(201).send(newItem)
+    })
+    
+
+    app.listen(3000)
+
+}
+
+main()
